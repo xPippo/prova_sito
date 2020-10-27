1785657b6f65d7b5ca090e78e529a2ff07bc6d6f
xPippo <69466252+xPippo@users.noreply.github.com>
2020-10-27 20:57:50 +0100

    Add files via upload

---
+++ generale.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+
+<html>
+  <head>
+    <title>Cop Libri</title>
+    <link rel="stylesheet" href="/sito_libri/css/sito_libri.css">
+    <link rel="stylesheet" href="/sito_libri/css/input_stile.css">
+    <link rel="stylesheet" href="/sito_libri/css/navbar_stile.css">
+    <link rel="stylesheet" href="/sito_libri/css/over_book.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  </head>
+  
+  <body>
+    <div class="header">
+      <h2>COP LIBRI</h2>
+    </div>
+    <a href="/sito_libri/php/compra/compra.php">
+      <div class = "contenitore" float = "left">
+        COMPRA
+      </div>
+    </a>
+    <a href="/sito_libri/php/vendi/vendi.php">
+      <div class = "contenitore" float = "right">
+        VENDI
+      </div>
+    </a>
+  </body>
+
+</html> 
+
